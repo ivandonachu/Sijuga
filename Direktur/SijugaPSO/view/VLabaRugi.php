5113b6a190ac863971cf4a056dc36195550329a4
DESKTOP-DIHSB0N\ASUS <ivandonachu13@gmail.com>
2024-01-15 13:55:04 +0700

    15 Januari 2024

--- Direktur/SijugaPSO/view/VLabaRugi.php
+++ Direktur/SijugaPSO/view/VLabaRugi.php
@@ -297,7 +297,7 @@ $laba_bersih_sebelum_pajak = $laba_kotor - $total_biaya_usaha_final;
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Laba Rugi Non PSO</title>
+    <title>Laba Rugi PSO</title>
 
     <!-- Custom fonts for this template-->
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
